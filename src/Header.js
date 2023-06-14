0e080b80efbf6ed84baede3b7f52eff41c51f384
Eric Krukowski <erickruk@amazon.com>
2023-06-14 00:41:06 -0400

    Create new/returning user logic, connect user to db

--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Header({ user }) {
-  return <div>{user ? <div>Welcome {user}!</div> : <div>Please login to use app functionality</div>}</div>;
+  return <div>{user ? <div>Welcome {user.name}!</div> : <div>Please login to use app functionality</div>}</div>;
 }
 
 export default Header;
--- src/Login.js
+++ src/Login.js
@@ -1,9 +1,55 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 function Login({ setIsLoggedIn, setUser }) {
   const [userForm, setUserForm] = useState("");
+  const [userList, setUserList] = useState([]);
+  const fetchURL = "http://localhost:3001/users/";
+  useEffect(() => {
+    const fetchUserList = async () => {
+      const userRaw = await fetch(fetchURL);
+      const userJSON = await userRaw.json();
+      setUserList(userJSON);
+    };
+    fetchUserList();
+  }, []);
   function handleLogin(e) {
     e.preventDefault();
-    setUser(userForm);
+    let foundUser = false;
+    let foundID = 0;
+    userList.forEach((u) => {
+      if (u.name === userForm) {
+        foundUser = true;
+        foundID = u.id;
+      }
+    });
+    const newUserList = JSON.parse(JSON.stringify(userList));
+    if (foundUser) {
+      async function getUser() {
+        const fetchResults = await fetch("" + fetchURL + foundID);
+        const fetchJSON = await fetchResults.json();
+        setUser(fetchJSON);
+      }
+      getUser();
+    } else {
+      setUserList(newUserList);
+      const newUser = {
+        name: userForm,
+        scores: [],
+      };
+      const fetchBody = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newUser),
+      };
+      async function addNewUser() {
+        const fetchResults = await fetch(fetchURL, fetchBody);
+        const fetchJSON = await fetchResults.json();
+        setUser(fetchJSON);
+      }
+      addNewUser();
+    }
+
     setIsLoggedIn(true);
     setUserForm("");
   }

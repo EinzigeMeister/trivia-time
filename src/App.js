09ae19a36463fb1232a2c1dabf2d21aa02a4cb96
Eric Krukowski <erickruk@amazon.com>
2023-06-13 21:49:46 -0400

    Add nav

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
+import { Switch, Route } from "react-router-dom";
 import Header from "./Header";
+import NavBar from "./NavBar";
+import Home from "./Home";
+import Login from "./Login";
+import NewQuiz from "./NewQuiz";
+import LoadQuiz from "./LoadQuiz";
+
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <Header />
     </div>
   );
--- src/NavBar.js
+++ src/NavBar.js
@@ -1,7 +1,17 @@
 import React from "react";
-
+import { NavLink } from "react-router-dom";
 function NavBar() {
-  return <h1>I'm the NavBar!</h1>;
+  return (
+    <nav>
+      <NavLink exact to="/">
+        Home
+      </NavLink>
+      <NavLink to="/login">Login</NavLink>
+      <NavLink to="/newquiz">New Quiz</NavLink>
+      <NavLink to="/loadquiz">Load Quiz</NavLink>
+      <NavLink to="/currentquiz">Current Quiz</NavLink>
+    </nav>
+  );
 }
 
 export default NavBar;

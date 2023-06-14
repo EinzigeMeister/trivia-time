26ac2488f55430a22cdce5c68f5312efb225f060
Eric Krukowski <erickruk@amazon.com>
2023-06-13 22:30:26 -0400

    Add alerts to inform users of login requirement

--- src/LoadQuiz.js
+++ src/LoadQuiz.js
@@ -1,7 +1,10 @@
 import React from "react";
 import { Redirect } from "react-router-dom";
 function LoadQuiz({ isLoggedIn }) {
-  if (!isLoggedIn) return <Redirect to="/login" />;
+  if (!isLoggedIn) {
+    window.alert("You must Login first, redirecting you to login");
+    return <Redirect to="/login" />;
+  }
   return <h1>This is the Load Quiz page</h1>;
 }
 export default LoadQuiz;
--- src/NewQuiz.js
+++ src/NewQuiz.js
@@ -1,7 +1,10 @@
 import React from "react";
 import { useHistory, Redirect } from "react-router-dom";
 function NewQuiz({ isLoggedIn }) {
-  if (!isLoggedIn) return <Redirect to="/login" />;
+  if (!isLoggedIn) {
+    window.alert("You must Login first, redirecting you to login");
+    return <Redirect to="/login" />;
+  }
   return <h1>This is a new Quiz</h1>;
 }
 export default NewQuiz;

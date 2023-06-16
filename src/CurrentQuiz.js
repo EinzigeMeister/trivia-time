bf93029695e454b58a134d770cbfa65b770844f2
Eric Krukowski <erickruk@amazon.com>
2023-06-15 23:08:26 -0400

    patch DB with user scores on form submission

--- src/CurrentQuiz.js
+++ src/CurrentQuiz.js
@@ -4,6 +4,7 @@ import React, { useState, useEffect } from "react";
 import { Redirect } from "react-router-dom";
 function CurrentQuiz({ isLoggedIn, user, setUser, quizID, quiz }) {
   const [quizAnswers, setQuizAnswers] = useState([]);
+  //Sets user's default answer to the first radio button
   useEffect(() => {
     if (!quiz) return;
     const initializeAnswers = [];
@@ -12,6 +13,7 @@ function CurrentQuiz({ isLoggedIn, user, setUser, quizID, quiz }) {
     });
     setQuizAnswers(initializeAnswers);
   }, [quiz]);
+
   if (!isLoggedIn) {
     window.alert("You must Login first, redirecting you to login");
     return <Redirect to="/login" />;
@@ -33,10 +35,24 @@ function CurrentQuiz({ isLoggedIn, user, setUser, quizID, quiz }) {
     quiz.forEach((q, index) => {
       if (q["correct_answer"] === quizAnswers[index]) correct++;
     });
+    //Update user state and DB with new quiz scores
     const scoreUpdate = [...user.scores];
     scoreUpdate[quizID] = [correct, quiz.length];
     const userUpdate = Object.assign({}, user, { scores: scoreUpdate });
-    setUser(userUpdate);
+    async function updateUserDB(){
+      const fetchURL = `http://localhost:3001/users/${user.id}`
+      const fetchBody={
+        "method": "PATCH",
+        "headers": {
+          "Content-Type": "application/json"
+        },
+        "body": JSON.stringify(userUpdate)
+      }
+      const userPatch = await fetch(fetchURL, fetchBody)
+      const userPatchObj = await userPatch.json()
+      setUser(userPatchObj);
+    }
+    updateUserDB()
     window.alert(`Quiz completed! You got ${correct} out of ${quiz.length} questions correct!`);
   }
   return (

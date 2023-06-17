10d2f37d06899ed91191950288d85f7251600b60
Eric Krukowski <erickruk@amazon.com>
2023-06-17 19:32:07 -0400

    updated username for blank entry

--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,9 @@
 import React from "react";
 
 function Header({ user }) {
-  return <div>{user ? <div>Welcome {user.name}!</div> : <div>Please login to use app functionality</div>}</div>;
+  let name = "";
+  if (user) user.name.length > 0 ? (name = user.name) : (name = "Anonymous");
+  return <div>{user ? <div>Welcome {name}!</div> : <div>Please login to use app functionality</div>}</div>;
 }
 
 export default Header;

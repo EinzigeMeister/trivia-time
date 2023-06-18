22cf007e503b48ffa967003bea3cbe93ee17ef8e
Eric Krukowski <erickruk@amazon.com>
2023-06-17 20:08:06 -0400

    fix special character formatting

--- src/NewQuiz.js
+++ src/NewQuiz.js
@@ -71,12 +71,12 @@ function NewQuiz({ user, setQuiz, setQuizID, loadLib, categoryList }) {
     return quizObj.results.map((q) => {
       const questionObj = {};
       //encode=base64 sends base64 back to ensure special characters are formatted properly, requires decoding via atob()
-      questionObj.name = atob(q.question);
+      questionObj.name = atob(q.question).replace("&amp;", /&/g).replace("&gt;", />/g).replace("&lt;", /</g).replace("&quot;", /"/g);
       questionObj.answers = [
-        atob(q["correct_answer"].replace(/&/g, "&amp;").replace(/>/g, "&gt;").replace(/</g, "&lt;").replace(/"/g, "&quot;")),
-        atob(q["incorrect_answers"][0]).replace(/&/g, "&amp;").replace(/>/g, "&gt;").replace(/</g, "&lt;").replace(/"/g, "&quot;"),
-        atob(q["incorrect_answers"][1]).replace(/&/g, "&amp;").replace(/>/g, "&gt;").replace(/</g, "&lt;").replace(/"/g, "&quot;"),
-        atob(q["incorrect_answers"][2]).replace(/&/g, "&amp;").replace(/>/g, "&gt;").replace(/</g, "&lt;").replace(/"/g, "&quot;"),
+        atob(q["correct_answer"]).replace("&amp;", /&/g).replace("&gt;", />/g).replace("&lt;", /</g).replace("&quot;", /"/g),
+        atob(q["incorrect_answers"][0]).replace("&amp;", /&/g).replace("&gt;", />/g).replace("&lt;", /</g).replace("&quot;", /"/g),
+        atob(q["incorrect_answers"][1]).replace("&amp;", /&/g).replace("&gt;", />/g).replace("&lt;", /</g).replace("&quot;", /"/g),
+        atob(q["incorrect_answers"][2]).replace("&amp;", /&/g).replace("&gt;", />/g).replace("&lt;", /</g).replace("&quot;", /"/g),
       ];
       questionObj["correct_answer"] = questionObj.answers[0];
       //Randomizes the array to prevent the first answer from always being the correct answer

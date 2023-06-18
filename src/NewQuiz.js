dbfc343452f67ae16a2d4bb3d22cfee2bf2da2c4
Eric Krukowski <erickruk@amazon.com>
2023-06-17 21:44:23 -0400

    reorganized functions, cleaned up code

--- src/NewQuiz.js
+++ src/NewQuiz.js
@@ -16,59 +16,7 @@ function NewQuiz({ user, setQuiz, setQuizID, loadLib, categoryList }) {
     window.alert("You must Login first, redirecting you to login");
     return <Redirect to="/login" />;
   }
-  const numQArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  //update new quiz in json-server
-  async function quizToDB(newQuiz) {
-    const fetchURL = "http://localhost:3001/quizLib/";
-    const fetchBody = {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(Object.assign({}, { questions: newQuiz })),
-    };
-    const dbUpdate = await fetch(fetchURL, fetchBody);
-    const dbUpdateObj = await dbUpdate.json();
-    setQuizID(dbUpdateObj.id);
-  }
-  function handleQuiz(e) {
-    e.preventDefault();
-    setDisabled(true);
-    async function fetchQuiz() {
-      //if All is selected, don't fetch by category, otherwise add a category filter
-      const categoryString = formData.category.id === 8 ? "" : `&&category=${formData.category.id}`;
-      const fetchURL = `https://opentdb.com/api.php?type=multiple&encode=base64${categoryString}&&amount=${formData.numberOfQuestions}&token=`;
-      const fetchRaw = await fetch(fetchURL);
-      const quizObj = await fetchRaw.json();
-      //response code 0 = success
-      if (quizObj["response_code"] === 0) {
-        const newQuiz = generateQuiz(quizObj);
-        setQuiz(newQuiz);
-        quizToDB(newQuiz);
-        setTimeout(() => {
-          history.push("/currentquiz");
-        }, 300);
-      } else {
-        console.log(quizObj["response_code"]);
-        window.alert("Unable to obtain quiz, try again later");
-      }
-    }
-    fetchQuiz();
-    //update library, setTimeout due to json-server restarting between POST quiz to DB & GET quiz library causing fetch errors
-    setTimeout(buttonTimeout, 400);
-    function buttonTimeout() {
-      loadLib();
-      setDisabled(false);
-    }
-  }
-  function handleCategoryChange(e) {
-    const categoryObj = {
-      id: e.target.value,
-      name: e.target[e.target.value - 8].label,
-    };
-    setFormData(Object.assign({}, formData, { category: categoryObj }));
-  }
-  function handleNumQuestionsChange(e) {
-    setFormData(Object.assign({}, formData, { numberOfQuestions: e.target.value }));
-  }
+  //reformats API information to use in db
   function generateQuiz(quizObj) {
     return quizObj.results.map((q) => {
       const questionObj = {};
@@ -86,6 +34,67 @@ function NewQuiz({ user, setQuiz, setQuizID, loadLib, categoryList }) {
       return questionObj;
     });
   }
+  //update new quiz in json-server
+  async function quizToDB(newQuiz) {
+    const fetchURL = "http://localhost:3001/quizLib/";
+    const fetchBody = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(Object.assign({}, { questions: newQuiz })),
+    };
+    const dbUpdate = await fetch(fetchURL, fetchBody);
+    const dbUpdateObj = await dbUpdate.json();
+    setQuizID(dbUpdateObj.id);
+  }
+  //Redirects to path after a short delay
+  function pushToPath(path) {
+    setTimeout(() => {
+      history.push(path);
+    }, 300);
+  }
+  //updates the quiz library and reactivates the submit button
+  function buttonTimeout() {
+    loadLib();
+    setDisabled(false);
+  }
+  async function fetchQuiz() {
+    //if All is selected, don't fetch by category, otherwise add a category filter
+    const categoryString = formData.category.id === 8 ? "" : `&&category=${formData.category.id}`;
+    const fetchURL = `https://opentdb.com/api.php?type=multiple&encode=base64${categoryString}&&amount=${formData.numberOfQuestions}&token=`;
+    const fetchRaw = await fetch(fetchURL);
+    const quizObj = await fetchRaw.json();
+    //response code 0 = success
+    if (quizObj["response_code"] === 0) {
+      const newQuiz = generateQuiz(quizObj);
+      setQuiz(newQuiz);
+      quizToDB(newQuiz);
+      pushToPath("/currentquiz");
+    } else {
+      console.log(quizObj["response_code"]);
+      window.alert("Unable to obtain quiz, try again later");
+    }
+  }
+  //Form submission
+  function handleQuiz(e) {
+    e.preventDefault();
+    setDisabled(true);
+    fetchQuiz();
+    //reactivates button AND updates quiz library
+    setTimeout(buttonTimeout, 400);
+  }
+
+  //Form updates
+  function handleCategoryChange(e) {
+    const categoryObj = {
+      id: e.target.value,
+      name: e.target[e.target.value - 8].label,
+    };
+    setFormData(Object.assign({}, formData, { category: categoryObj }));
+  }
+  function handleNumQuestionsChange(e) {
+    setFormData(Object.assign({}, formData, { numberOfQuestions: e.target.value }));
+  }
+
   return (
     <>
       <h1>Create a new Quiz!</h1>
@@ -101,7 +110,7 @@ function NewQuiz({ user, setQuiz, setQuizID, loadLib, categoryList }) {
         <div>
           <label>Number of questions: </label>
           <select onChange={handleNumQuestionsChange} value={formData.numberOfQuestions}>
-            {numQArr.map((n) => (
+            {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((n) => (
               <option key={n} value={n} label={n} />
             ))}
           </select>
